2ec2d3f7dc49272f9ded11812f97a815e9dbcd6e
Kaleb Kuo <klkuo.cx@gmail.com>
2020-09-21 21:20:33 +0800

    feat: add update record function
    - add "edit button" in index.hbs
    - set route to /records/:id/edit
    - add edit.hbs
    - set route to edit record in DB and redirect to index
    - require handlebars-helper to optimize UX

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const hbshelpers = require('handlebars-helpers')
 const bodyParser = require('body-parser')
 
 const Record = require('./models/record')
 const Category = require('./models/category')
+const category = require('./models/category')
 
 const app = express()
 const port = 3000
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 const db = mongoose.connection
 db.on('error', () => {
   console.error('MongoDB error 0_0')
@@ -21,7 +23,7 @@ db.once('open', () => {
 })
 
 // Set view engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ helpers: hbshelpers(), defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // Set middleware
@@ -58,6 +60,53 @@ app.post('/records/new', (req, res) => {
           category.save()
         })
         .then(() => res.redirect('/'))
+        .catch(error => console.error(error))
+    })
+    .catch(error => console.error(error))
+})
+
+// Set routes to edit record
+app.get('/records/:id/edit', (req, res) => {
+  let categories = []
+  Category.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(all => categories = all)
+    .catch(error => console.error(error))
+
+  const id = req.params.id
+  Record.findById(id)
+    .populate('category')
+    .lean()
+    .then(record => res.render('edit', { record, categories }))
+    .catch(error => console.error(error))
+})
+
+app.post('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const update = req.body
+  // remove this record from old category
+  Record.findById(id)
+    .then(record => {
+      Category.findById(record.category)
+        .then(category => {
+          category.records = category.records.filter(record => record.toString() !== id)
+          category.save()
+        })
+    })
+
+  // assign category id in update object
+  Category.findOne({ title: update.category })
+    .then(category => {
+      update.category = category._id
+
+      // update record
+      Record.findByIdAndUpdate(id, update, { new: true })
+        .then(record => {
+          category.records.push(record._id)
+          category.save()
+        })
+        .then(() => res.redirect(`/`))
         .catch(error => console.error(error))
     })
     .catch(error => console.error(error))
