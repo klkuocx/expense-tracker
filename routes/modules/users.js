4d1dfa5bb793e7ad5e5aab43ecfc3e31a8ee0e63
Kaleb Kuo <klkuo.cx@gmail.com>
2020-11-10 15:58:13 +0800

    feat: add login/logout and optimize register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('passport')
+
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', (req, res) => {
+router.post('/register', (req, res, next) => {
   const { name, email, password, confirmPassword } = req.body
 
   User.findOne({ email }).then(user => {
@@ -33,13 +36,17 @@ router.post('/register', (req, res) => {
       email,
       password
     })
-      .then(() => res.redirect('/'))
+      .then(() => next())
       .catch(err => console.log(err))
   })
-})
+}, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/logout', (req, res) => {
-
+  req.logout
+  res.redirect('/users/login')
 })
 
 module.exports = router
